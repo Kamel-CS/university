df4b5612702962158508d89f3fa5bc4831f54d3c
Kamel <kameldemri00@gmail.com>
2024-10-16 06:18:12 +0100

    Add save_books() function

--- FSDS/1-PT/file/file.c
+++ FSDS/1-PT/file/file.c
@@ -1,2 +1,26 @@
 #include "../global/global.h"
 #include "../struct/struct.h"
+
+
+void save_books(Book* head, string* file_name) {
+    FILE *file = fopen(file_name, "w");
+    if (file == NULL)
+        return;
+
+    Book* current = head;
+    while (current != NULL) {
+        fprintf(file, "%s,%s,%d\n", file_name);
+        current = current->next;
+    }
+
+    fclose(file_name);
+}
+
+
+Book* load_books(string* file_name) {
+    FILE *file = fopen(file_name, "r");
+    if (file == NULL)
+        return NULL;
+
+    Book* head = NULL, current = NULL;
+}
--- FSDS/1-PT/file/file.h
+++ FSDS/1-PT/file/file.h
@@ -2,6 +2,8 @@
 #define FILE_H
 
 // function prototypes
+void save_books(Book* head, string* file_name);
+Book* load_books(string* file_name);
 
 
 #endif
